7aff861c4204a2745f8017a2c8ebb44375b2e4d1
ahmedfathy0-0 <ahmedfathi20044002@gmail.com>
2026-02-07 16:14:31 +0200

    fix: update Prisma configuration and service to use environment variables for database connection

--- src/modules/prisma/prisma.service.ts
+++ src/modules/prisma/prisma.service.ts
@@ -6,6 +6,14 @@ export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
+  constructor() {
+    super({
+      adapter: {
+        connectionString: process.env.DATABASE_URL,
+      },
+    });
+  }
+
   async onModuleInit() {
     try {
       await this.$connect();
